12f09c226f135850733962940df0a10f37e251bf
velvet-noir <this.is.romniko@gmail.com>
2025-06-04 16:31:24 +0700

    сделал шаблоны

--- server/urls.py
+++ server/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from server import views
 
 urlpatterns = [
-    path("", views.GetOrders),
-    path("order/<int:id>/", views.GetOrder, name="order_url"),
+    path('', views.service_list, name='service_list'),
+    path('service/<int:service_id>/', views.service_detail, name='service_detail'),
 ]
